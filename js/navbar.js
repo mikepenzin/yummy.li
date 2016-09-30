1019e47bcb681afb6933adb73bcffc00d409081b
mikepenzin <mike.penzin@gmail.com>
2016-09-30 18:48:32 +0300

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,22 @@
-(function($){$(document).ready(function(){$(".navbar").hide();$(function(){$(window).scroll(function(){if($(this).scrollTop()>100){$('.navbar').fadeIn();}else{$('.navbar').fadeOut();}});});});}(jQuery));
+(function ($) {
+  $(document).ready(function(){
+    
+	// hide .navbar first
+	$(".navbar").hide();
+	
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 100) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+
+	
+	});
+
+});
+  }(jQuery));
