8a429f62d678026624234ef7e302911a31496e3e
mikepenzin <mike.penzin@gmail.com>
2016-10-07 16:13:34 +0300

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,26 @@
 (function ($) {
   $(document).ready(function(){
     
-	// hide .navbar first
-	$(".navbar").hide();
+	// // hide .navbar first
+	// $(".navbar").hide();
 	
 	// fade in .navbar
 	$(function () {
+		
 		$(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
 			if ($(this).scrollTop() > 100) {
-				$('.navbar').fadeIn();
+				$('.navbar-inverse').fadeIn();
+				$('.navbar-inverse').css('background-color', '#393e41');
+				
+				// $('.navbar-header').css('background-color', '#393e41').fadeIn();
+
 			} else {
-				$('.navbar').fadeOut();
+				$('.navbar-inverse').css('background-color', 'transparent');
+				
+				// $('.navbar-header').css('background-color', 'transparent');
 			}
 		});
-
 	
 	});
 
