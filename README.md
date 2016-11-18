b94a597d76d9ae1f64528ed592f352169d8c351d
mikepenzin <mike.penzin@gmail.com>
2016-11-18 23:07:06 +0000

    Made major changes - v1.2.0

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Search recipes using ingredients you already have in the kitchen!
 
+v. 1.2.0 - Initial release
+
+1. Major bugs were fixed
+2. Added show page for recipes by id
+3. Made major changes in design concept of all pages (transparent navbar and better css file structure)
+
 v. 1.1.0 - Initial release
 
 1. Added another api - Food2Fork - http://food2fork.com/about/api
--- routes/recipes.js
+++ routes/recipes.js
@@ -9,6 +9,7 @@ router.get("/", function(req, res){
 
 // Used Environment variable (process.env.API_URL) for my personal API key from Food2Fork
 
+//SHOW search results for recipes - by ingridients 
 router.get("/q", function(req, res){
     var search = req.query.search;
     var page = Number(req.query.page) || 1;
@@ -19,7 +20,7 @@ router.get("/q", function(req, res){
         if (result.statusCode == 200) {
             var data = JSON.parse(result.body);
             data = data.recipes;
-            if(data.length > 0){   
+            if(data !== undefined || data.length > 0){   
                //this array is not empty 
                res.render("recipe/search", {data:data, q:search, page:page});
             }else{
@@ -29,14 +30,30 @@ router.get("/q", function(req, res){
           } else {
             console.log("Something whent wrong!");
             console.log(result.status, result.headers);
+            res.redirect("back");
           }    
     });
 });
 
+//SHOW - recipe by recipe id
 router.get("/recipe/:recipe_id", function(req, res){
-    console.log(req.params.id);
-    res.render("recipe/show");
+    console.log(req.params.recipe_id);
+    var url = "https://community-food2fork.p.mashape.com/get?key=" + process.env.API_URL + "&rId=" + req.params.recipe_id;
+    unirest.get(url)
+    .header("X-Mashape-Key", "Fj5xGp8OhGmshrg45yEPk5IvGe0xp1DCBIFjsnLHD4OsQxCvPD")
+    .header("Accept", "application/json")
+    .end(function (result) {
+        if (result.statusCode == 200) {
+            var data = JSON.parse(result.body);
+            data = data.recipe;
+            console.log(data); 
+            res.render("recipe/show", {recipe:data});
+          } else {
+            console.log("Something whent wrong!");
+            console.log(result.status, result.headers);
+            res.redirect("back");
+          }    
+    });
 });
-
 
 module.exports = router;
